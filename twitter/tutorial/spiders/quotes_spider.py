94b3a12ef2914dd87b2eb99617e2a4af263e537d
vagrant <vagrant@vagrant.vm>
2017-05-19 20:08:36 +0000

    parsing built, with bugs

--- twitter/tutorial/spiders/quotes_spider.py
+++ twitter/tutorial/spiders/quotes_spider.py
@@ -1,6 +1,7 @@
 import logging
 import scrapy
 import datetime
+import json
 
 
 class QuotesSpider(scrapy.Spider):
@@ -16,13 +17,26 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        logging.info('Inside the parse function')
-        # full_text = response.body
+        logging.info('Begin parsing the response')
+        # grab html that has a class of tweet-text for parsing
+        all_tweet_text = response.xpath('//*[contains(@class, "tweet-text")]/text()').extract()
+        logging.info('ALL TWEET TEXT')
+        print(all_tweet_text)
+
         date = datetime.datetime.today().strftime('%Y-%m-%d')
         site = response.url.split("/")[-1]
         site_date = site + date
         filename = '%s-twitter-crawl.html' % site_date
+        cleaned_array = []
+        all_cleaned_by_website = dict()
+        for tweet in all_tweet_text:
+            logging.info('INSIDE THE CLEANING LOOP')
+            if tweet[0].isalpha():
+                cleaned_array.append(tweet)
+
+        all_cleaned_by_website.update({site_date : cleaned_array})
+        # possibly generate dict keyed on website name. push into large json
 
         with open(filename, 'wb') as f:
-            f.write(response.body)
+            f.write(all_cleaned_by_website)
         self.log('Saved file %s' % filename)
