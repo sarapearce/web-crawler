993b2ffde47de0dc68215b767803d8b15224c42d
vagrant <vagrant@vagrant.vm>
2017-05-22 14:15:04 +0000

    printing correct tuple, not converting to json correctly

--- twitter/tutorial/spiders/quotes_spider.py
+++ twitter/tutorial/spiders/quotes_spider.py
@@ -32,19 +32,41 @@ class QuotesSpider(scrapy.Spider):
         filename = '%s-twitter-crawl.html' % site_date
 
 
-        # loop and grab proper nouns
+        # loop, grab, and count proper nouns
         proper_nouns = []
         for tweet in all_tweet_text:
             words = tweet.split()
 
             for word in words:
+                if word[0].isupper():
+                    proper_nouns.append(word)
 
-             if word[0].isupper():
-                 proper_nouns.append(word)
+        # print('PROPER NOUNS ARRAY')
+        # print(proper_nouns)
 
-        print('PROPER NOUNS ARRAY')
-        print(proper_nouns)
+        # do counts on elements in the proper_nouns list
+        distinct_proper_nouns = set(proper_nouns)
 
-        # with open(filename, 'wb') as f:
-        #     f.write(all_cleaned_by_website)
-        # self.log('Saved file %s' % filename)
+        noun_count = []
+        counts_by_site_date = []
+        for noun in distinct_proper_nouns:
+            # delete possessive 's on proper nouns for counting
+            if "/'s" in noun:
+                noun = string.replace(noun, "/'s", " ")
+
+            count = proper_nouns.count(noun)
+            noun_count.append((noun, count))
+
+        counts_by_site_date.append((site_date, noun_count))
+
+
+        # print('COUNTS BY SITE DATE')
+        # print(counts_by_site_date)
+
+        jsonObj = json.dumps(dict(counts_by_site_date))
+        print(jsonObj)
+
+        # write output to a file
+         # with open(filename, 'wb') as f:
+         #     f.write(counts_by_site_date)
+         # self.log('Saved file %s' % filename)
