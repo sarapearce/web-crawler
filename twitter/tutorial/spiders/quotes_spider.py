6cf6cd73e3d72c272455cb5e1e315d22424ddc78
vagrant <vagrant@vagrant.vm>
2017-06-07 23:53:41 +0000

    commit before making a new bot out of the tutorial example

--- twitter/tutorial/spiders/quotes_spider.py
+++ twitter/tutorial/spiders/quotes_spider.py
@@ -22,6 +22,7 @@ class QuotesSpider(scrapy.Spider):
         # grab html that has a class of tweet-text for parsing
         all_tweet_text = response.xpath('//*[contains(@class, "tweet-text")]/text()').extract()
 
+        #
         date = datetime.datetime.today().strftime('%Y-%m-%d')
         site = response.url.split("/")[-1]
 
@@ -38,35 +39,53 @@ class QuotesSpider(scrapy.Spider):
             words = tweet.split()
 
             for word in words:
+                # use the uppercase first letter as the flag for a proper noun. not a robust assumption, i know
                 if word[0].isupper():
                     proper_nouns.append(word)
 
         # print('PROPER NOUNS ARRAY')
         # print(proper_nouns)
 
-        # do counts on elements in the proper_nouns list
+        # get proper nouns with no repeats
         distinct_proper_nouns = set(proper_nouns)
 
         noun_count = []
         counts_by_site_date = []
         for noun in distinct_proper_nouns:
-            # delete possessive 's on proper nouns for counting
-            if "/'s" in noun:
-                noun = string.replace(noun, "/'s", " ")
+            # # clean nouns for counting
+            # if "'s" in noun:
+            #     noun = noun.replace("'s", "")
+            #
+            # if "\u2019s" in noun:
+            #     noun = noun.replace("\u2019s", "")
+            #
+            # if "!" in noun:
+            #     noun = noun.replace("!", "")
 
+            # this wont work yet because the proper_nouns array does not have clean data
             count = proper_nouns.count(noun)
             noun_count.append((noun, count))
 
-        counts_by_site_date.append((site_date, noun_count))
+        # counts_by_site_date.append((site_date, noun_count))
 
 
-        # print('COUNTS BY SITE DATE')
-        # print(counts_by_site_date)
+        print('COUNTS BY SITE DATE')
+        print(counts_by_site_date)
 
-        jsonObj = json.dumps(dict(counts_by_site_date))
-        print(jsonObj)
+        print(json.dumps({'4': 5, '6': 7}, sort_keys=True, indent=4))
 
-        # write output to a file
+        # json._default_encoder
+
+        counts_by_site_date_dict = dict(counts_by_site_date)
+        jsonObj = json.dumps(counts_by_site_date_dict)
+        jsonObj_clean_left = str.replace(jsonObj, "[", "{")
+        jsonObj_clean = str.replace(jsonObj_clean_left, "]", "}")
+        # print(jsonObj_clean)
+
+        # build the giant json blob for sending to the front end
+        # complete_json_blob = {}
+        # complete_json_blob[site_date] =
+         # write output to a file
          # with open(filename, 'wb') as f:
          #     f.write(counts_by_site_date)
          # self.log('Saved file %s' % filename)
